25751b17e3a3e74164896df873fa98af14d9e0d8
Subhas19 <73435520+Subhas19@users.noreply.github.com>
2021-10-27 21:20:05 -0500

    bonus modified

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', mydataRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/users', computationRouter);
+app.use('/computation', computationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
